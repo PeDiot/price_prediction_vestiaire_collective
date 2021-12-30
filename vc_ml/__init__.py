2075574426c57ce6801da43d54a2a6066a1db31e
Pierre-Emmanuel <p.emmanuel.diot@gmail.com>
2021-12-30 19:52:00 +0100

    Modify __init__.

--- vc_ml/__init__.py
+++ vc_ml/__init__.py
@@ -10,15 +10,15 @@ In [1]: from vc_ml import (
    ...: Target,
    ...: )
 In [2]: config = load_config()
->>> X_tr, y_tr = load_data(file_name="train.pkl", target=Target.PRICE)
-In [3]: training = Training(
+In [3]: X_tr, y_tr = load_data(file_name="train.pkl", target=Target.PRICE)
+In [4]: training = Training(
    ...: X=X_tr,
    ...: y=y_tr,
    ...: config=config,
    ...: cv=3
    ...: )
-In [4]: training
-Out[4]: 
+In [5]: training
+Out[5]: 
 Training(X=[[0 1 0 ... 0 1 0]
  [0 1 0 ... 0 0 0]
  [0 1 1 ... 0 1 0]
@@ -26,12 +26,12 @@ Training(X=[[0 1 0 ... 0 1 0]
  [0 1 0 ... 0 1 0]
  [0 1 0 ... 0 1 0]
  [1 0 0 ... 0 1 0]], y=[300.    50.35 125.   ...  85.   325.   249.  ], config=Config(lr=LREstimator(fit_intercept=[True]), ridge=RidgeEstimator(alpha=[1.0], fit_intercept=[True]), tree=TreeEstimator(max_depth=[None], min_samples_split=[2], min_samples_leaf=[1], max_features=['auto'], ccp_alpha=[0.0]), rf=RFEstimator(n_estimators=[100], max_depth=[None], min_samples_split=[2], min_samples_leaf=[1], max_features=['auto'], max_samples=[1.0], oob_score=[True]), gb=GBEstimator(n_estimators=[100], min_samples_split=[2], min_samples_leaf=[1], max_depth=[3], loss=['squared_error'], learning_rate=[0.1], criterion=['friedman_mse'], tol=[0.001]), mlp=MLPEstimator(hidden_layer_sizes=[[100]], max_iter=[200], activation=['relu'], solver=['adam'], learning_rate_init=[0.001])), cv=3, n_pcs=[40])
-In [5]: p = training.make_pipeline()
+In [6]: p = training.make_pipeline()
 
-In [6]: g = training.build_grid_search(p)
+In [7]: g = training.build_grid_search(p)
 
-In [7]: g
-Out[7]: 
+In [8]: g
+Out[8]: 
 GridSearchCV(cv=3,
              estimator=Pipeline(steps=[('pca', PCA()),
                                        ('model', EstimatorSwitcher())]),
@@ -46,7 +46,7 @@ GridSearchCV(cv=3,
                          {'...
                           'model__estimator__tol': [0.001],
 
-In [8]: training.fit(g)
+In [9]: training.fit(g)
 Fitting 3 folds for each of 6 candidates, totalling 18 fits
 ...         
 In [10]: from vc_ml import display_results
