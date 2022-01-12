cb7f636f7b15192ea1fa4345815213cc81ee3043
Pierre-Emmanuel <p.emmanuel.diot@gmail.com>
2022-01-12 12:07:29 +0100

    Add condition on estimators.

--- vc_ml/estimators.py
+++ vc_ml/estimators.py
@@ -58,6 +58,12 @@ class RidgeEstimator:
     alpha: Optional[list[float]] = field(default_factory=lambda: [1.0])  
     fit_intercept: Optional[list[bool]] = field(default_factory=lambda: [True])  
 
+    def __post_init__(self):
+        """Check whether alpha is a list of positive floats."""
+        for alpha in self.alpha:
+            if alpha < 0 or type(alpha) != float:
+                raise ValueError("alpha must be a list of positive floats.")
+
     def __call__(self) -> Dict:
         return {
             "estimator": Ridge(), 
@@ -74,6 +80,24 @@ class TreeEstimator:
     min_samples_leaf: Optional[list[int]] = field(default_factory=lambda: [1])  
     max_features: Optional[list[str]] = field(default_factory=lambda: ["auto"])  
     ccp_alpha: Optional[list[float]] = field(default_factory=lambda: [0.0])  
+
+    def __post_init__(self):
+        """Check parameters."""
+        for max_depth in self.max_depth:
+            if type(max_depth) != type(None):
+                if type(max_depth) != int or max_depth <= 0:
+                    raise ValueError("'max_depth' must be a list of strictly positive intergers.")
+        for min_samples_split, min_samples_leaf in zip(self.min_samples_split, self.min_samples_leaf):
+            if type(min_samples_split) != int or min_samples_split <= 0:
+                raise ValueError("min_samples_split' must be a list of strictly positive intergers.")
+            if type(min_samples_leaf) != int or min_samples_leaf <= 0:
+                raise ValueError("min_samples_leaf' must be a list of strictly positive intergers.")
+        for ccp in self.ccp_alpha: 
+            if type(ccp) != float or ccp < 0.0:
+                raise ValueError("'ccp_alpha' must be a list of positive floats.")
+        for max_features in self.max_features:
+            if max_features not in ("auto", "sqrt", "log2", None):
+                raise ValueError("'max_features' must take the following values: 'auto', 'sqrt', 'log2' or None.")
 
     def __call__(self) -> Dict:
         return  {
@@ -98,6 +122,33 @@ class RFEstimator:
     max_samples: Optional[list[float]] = field(default_factory=lambda: [1.0])  
     oob_score: Optional[list[bool]] = field(default_factory=lambda: [True])   
 
+    def __post_init__(self):
+        """Check parameters."""
+        for n_estimators in self.n_estimators:
+            if type(n_estimators) != int or n_estimators <= 0:
+                raise ValueError("'n_estimators' must be a list of strictly positive intergers.")
+        for criterion in self.criterion:
+            if criterion not in ("squared_error", "absolute_error", "poisson"):
+                raise ValueError("'criterion' must take the following values: 'squared_error', 'absolute_error' or 'poisson'.")
+        for max_depth in self.max_depth:
+            if type(max_depth) != type(None):
+                if type(max_depth) != int or max_depth <= 0:
+                    raise ValueError("'max_depth' must be a list of strictly positive intergers.")
+        for min_samples_split, min_samples_leaf in zip(self.min_samples_split, self.min_samples_leaf):
+            if type(min_samples_split) != int or min_samples_split <= 0:
+                raise ValueError("min_samples_split' must be a list of strictly positive intergers.")
+            if type(min_samples_leaf) != int or min_samples_leaf <= 0:
+                raise ValueError("min_samples_leaf' must be a list of strictly positive intergers.")
+        for max_features in self.max_features:
+            if max_features not in ("auto", "sqrt", "log2", None):
+                raise ValueError("'max_features' must take the following values: 'auto', 'sqrt', 'log2' or None.")
+        for max_samples in self.max_samples:
+            if type(max_samples) != float or max_samples < 0.0 or max_samples > 1.:
+                raise ValueError("'max_samples' must be a list of floats between 0 and 1.")
+        for oob_score in self.oob_score:
+            if type(oob_score) != bool:
+                raise ValueError("'oob_score' must be a list of boolean.")
+
     def __call__(self) -> Dict:
         return {
             "estimator": RandomForestRegressor(), 
@@ -121,7 +172,34 @@ class GBEstimator:
     loss: Optional[list[str]] = field(default_factory=lambda: ["squared_error"])  
     learning_rate: Optional[list[float]] = field(default_factory=lambda: [.1])  
     criterion: Optional[list[str]] = field(default_factory=lambda: ["friedman_mse"])  
-    tol: Optional[list[float]] = field(default_factory=lambda: [.001])  
+    tol: Optional[list[float]] = field(default_factory=lambda: [.001]) 
+
+    def __post_init__(self):
+        """Check parameters."""
+        for n_estimators in self.n_estimators:
+            if type(n_estimators) != int or n_estimators <= 0:
+                raise ValueError("'n_estimators' must be a list of strictly positive intergers.")
+        for criterion in self.criterion:
+            if criterion not in ("squared_error", "friedman_mse", "mse", "mae"):
+                raise ValueError("'criterion' must take the following values: 'squared_error', 'friedman_mse', 'mse or 'mae'.")
+        for max_depth in self.max_depth:
+            if type(max_depth) != type(None):
+                if type(max_depth) != int or max_depth <= 0:
+                    raise ValueError("'max_depth' must be a list of strictly positive intergers.")
+        for min_samples_split, min_samples_leaf in zip(self.min_samples_split, self.min_samples_leaf):
+            if type(min_samples_split) != int or min_samples_split <= 0:
+                raise ValueError("min_samples_split' must be a list of strictly positive intergers.")
+            if type(min_samples_leaf) != int or min_samples_leaf <= 0:
+                raise ValueError("min_samples_leaf' must be a list of strictly positive intergers.")
+        for loss in self.loss: 
+            if loss not in ("squared_error", "absolute_error", "huber", "quantile"):
+                raise ValueError("'loss' must take the following values: 'squared_error', 'absolute_error', 'huber' or 'quantile'.")
+        for learning_rate in self.learning_rate:
+            if learning_rate <= 0 or type(learning_rate) != float: 
+                raise ValueError("'learning_rate' must be a list of strictly positive floats.")
+        for tol in self.tol:
+            if tol <= 0 or type(tol) != float:
+                raise ValueError("'tol' must be a list of strictly positive float.")        
 
     def __call__(self) -> Dict:
         return {
@@ -145,6 +223,27 @@ class MLPEstimator:
     activation: Optional[list[str]] = field(default_factory=lambda: ["relu"])  
     solver: Optional[list[str]] = field(default_factory=lambda: ["adam"])  
     learning_rate_init: Optional[list[float]] = field(default_factory=lambda: [.001])  
+
+    def __post_init__(self): 
+        "Check parameters."
+        for hidden_layer_sizes in self.hidden_layer_sizes:
+            if type(hidden_layer_sizes) != list:
+                raise ValueError("'hidden_layer_sizes' must be a list.")
+            for layer in hidden_layer_sizes:
+                if type(layer) != int or layer <= 0:
+                    raise ValueError("The number of neurons in each layer must be a strictly positive intergers.")
+        for max_iter in self.max_iter:
+            if type(max_iter) != int or max_iter <= 0:
+                raise ValueError("'max_iter' must be a list of strictly positive intergers.")
+        for activation in self.activation: 
+            if activation not in ("identity", "logistic", "tanh", "relu"):
+                raise ValueError("'activation' must take the following values: 'identity', 'logistic', 'tanh' or 'relu'.")
+        for solver in self.solver: 
+            if solver not in ("lbfgs", "sgd", "adam"):
+                raise ValueError("'solver' must take the following values: 'lbfgs', 'sgd' or'adam'.")
+        for learning_rate_init in self.learning_rate_init:
+            if learning_rate_init <= 0 or type(learning_rate_init) != float:
+                raise ValueError("'learning_rate_init' must be a strictly positive floats.")
 
     def __call__(self) -> Dict:
         return {
