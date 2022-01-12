85ec525eee41310cd4ff1af50071b52f8e3f37d1
Pierre-Emmanuel <p.emmanuel.diot@gmail.com>
2022-01-13 00:14:12 +0100

    Fix errors.

--- vc_ml/__main__.py
+++ vc_ml/__main__.py
@@ -44,12 +44,32 @@ from .selection import (
 
 from rich import print
 
+print(" "*100)
+print("Usage example of the 'vc_ml' library which aims to automate model training.")
+print(" "*100)
+print("-"*100)
+print(" "*100)
+print("Data used in models")
+print(" "*100)
+
 X_tr, y_tr = load_feature_vector(file_name="train.pkl"), load_target(file_name="train.pkl")
 print(f"Feature vector: {X_tr}")
 print(f"Target: {y_tr}")
+print(" "*100)
+print("-"*100)
+
+print(" "*100)
+print("Model configuration")
+print(" "*100)
 
 config = load_config(file_name="config_init.yaml")
 print(config)
+print(" "*100)
+print("-"*100)
+
+print(" "*100)
+print("Model training")
+print(" "*100)
 
 train_models(
     X_tr=X_tr,
@@ -58,3 +78,12 @@ train_models(
     cv=5,
     comp_grid=[60]
 )
+print(" "*100)
+print("-"*100)
+
+print(" "*100)
+print("Model selection")
+
+cv_results = get_cv_results(files_path=get_files_path())
+print(f"""Cross-validation results\n {cv_results}""")
+print(f"Best estimator: {get_best_estimator(cv_results)}")
